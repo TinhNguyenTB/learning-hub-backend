1c9502bd719885e5f074aaa5f05a8f53abaa4ed9
TinhNguyenTB <tinh271103@gmail.com>
2024-09-04 16:21:53 +0700

    init project

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsEmail, IsNotEmpty, IsOptional } from "class-validator"
+
+export class CreateUserDto {
+    @IsEmail()
+    email: string
+
+    @IsNotEmpty()
+    password: string
+
+    @IsNotEmpty()
+    name: string
+
+    @IsOptional()
+    image: string
+}
