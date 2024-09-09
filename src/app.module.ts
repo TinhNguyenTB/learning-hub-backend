69c4fd52f47c25bafbb95df5df6880ba4afc3978
TinhNguyenTB <tinh271103@gmail.com>
2024-09-09 08:26:55 +0700

    Generate modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,15 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '@/modules/users/users.module';
+import { CategoriesModule } from '@/modules/categories/categories.module';
+import { CoursesModule } from '@/modules/courses/courses.module';
+import { LevelsModule } from '@/modules/levels/levels.module';
+import { ProgressModule } from '@/modules/progress/progress.module';
+import { PurchasesModule } from '@/modules/purchases/purchases.module';
+import { ResourcesModule } from '@/modules/resources/resources.module';
+import { SectionsModule } from '@/modules/sections/sections.module';
+import { SubcategoriesModule } from '@/modules/subcategories/subcategories.module';
+import { VideosModule } from '@/modules/videos/videos.module';
 import { AuthModule } from '@/auth/auth.module';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from '@/auth/passport/jwt-auth.guard';
@@ -15,6 +24,15 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     AuthModule,
+    CategoriesModule,
+    CoursesModule,
+    LevelsModule,
+    ProgressModule,
+    PurchasesModule,
+    ResourcesModule,
+    SectionsModule,
+    SubcategoriesModule,
+    VideosModule,
     MailerModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
