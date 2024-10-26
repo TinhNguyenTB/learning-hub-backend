ea8aa14d85786912557f989735a979841af0a1cd
TinhNguyenTB <tinh271103@gmail.com>
2024-10-26 11:36:21 +0700

    send email payment confirmation

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -1,13 +1,15 @@
 import { stripe } from '@/lib/stripe';
 import { PrismaService } from '@/prisma.service';
+import { MailerService } from '@nestjs-modules/mailer';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import Stripe from "stripe";
+import Stripe from 'stripe';
 
 @Injectable()
 export class WebhookService {
     constructor(
         private prisma: PrismaService,
+        private readonly mailerService: MailerService,
         private configService: ConfigService
     ) { }
 
@@ -18,28 +20,56 @@ export class WebhookService {
                 rawBody,
                 signature,
                 this.configService.get<string>("STRIPE_WEBHOOK_SECRET")
-            )
+            );
         } catch (error: any) {
-            throw new BadRequestException(`Webhook signature verification failed:: ${error.message}`)
+            throw new BadRequestException(`Webhook signature verification failed: ${error.message}`);
         }
 
-        const session = event.data.object as Stripe.Checkout.Session
-        const customerId = session?.metadata?.customerId
-        const courseId = session?.metadata?.courseId
+        const session = event.data.object as Stripe.Checkout.Session;
+
+        const customerId = session?.metadata?.customerId;
+        const courseId = session?.metadata?.courseId;
+
+        if (!session) {
+            throw new BadRequestException('Session not found');
+        }
+
+        let courseName: string | undefined;
+        try {
+            const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
+            courseName = lineItems.data[0]?.description;
+        } catch (error: any) {
+            throw new BadRequestException(`Failed to retrieve line items: ${error.message}`);
+        }
+
         if (event.type === 'checkout.session.completed') {
             if (!customerId || !courseId) {
-                throw new BadRequestException(`Missing metadata`)
+                throw new BadRequestException('Missing metadata');
             }
+
             await this.prisma.purchase.create({
                 data: {
                     customerId,
                     courseId
                 }
-            })
+            });
+
+            await this.mailerService.sendMail({
+                to: session.customer_details.email,
+                subject: 'Payment Confirmation ✔',
+                template: 'checkout',
+                context: {
+                    customerName: session.customer_details.name,
+                    amount: (session.amount_total / 100).toFixed(2),
+                    currency: session.currency.toUpperCase(),
+                    transactionId: session.id,
+                    courseName: courseName,
+                }
+            });
+        } else {
+            throw new BadRequestException(`Unhandled event type: ${event.type}`);
         }
-        else {
-            throw new BadRequestException(`Unhandled event type: ${event.type}`)
-        }
-        return "Success"
+
+        return "Success";
     }
 }
